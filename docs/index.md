078c78b97c54ff2e7193bf6e0be210aae6ad02e6
Kenji Saito <k.saito.1985@gmail.com>
2017-12-25 11:33:29 +0100

    update thumbnail size

--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,4 @@
 
 ### [app00](./app00/index.html)
 
-![](./app00/thumbnail.png)
+[![](./app00/thumbnail.png)](./app00/index.html)
--- node/screenshot.js
+++ node/screenshot.js
@@ -9,7 +9,7 @@ const puppeteer = require('puppeteer');
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	await page.setViewport({ width: 800, height: 450 });
+	await page.setViewport({ width: 640, height: 360 });
 	await page.goto('http://localhost:1234/');
 	await page.screenshot({ path: `docs/${dir}/thumbnail.png` });
 
