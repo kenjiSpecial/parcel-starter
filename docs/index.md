73af4916142a376520480f52b53413271cfc24c4
Kenji <k.saito.1985@gmail.com>
2018-05-01 11:55:16 +0200

    init just-do-it

--- docs/index.md
+++
@@ -1,4 +0,0 @@
-
-### [app00](./app00/index.html)
-
-[![](./app00/thumbnail.png)](./app00/index.html)
--- examples/00-example/components/shaders/shader-vert.glsl
+++
@@ -1,6 +0,0 @@
-attribute vec4 a_position;
-uniform float uTheta;
-
-void main() {
-    gl_Position = a_position + vec4(0.0 * cos(uTheta), 0.0 * sin(uTheta), 0.0, 0.0);
-}
