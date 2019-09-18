06f9d52b194d686fc80c57d60dab5fe536689889
Kenji Saito <k.saito.1985@gmail.com>
2019-09-18 15:01:41 +0900

    remove unncessary files

--- dev/demo.ts
+++ dev/demo.ts
@@ -1,23 +1,5 @@
 import { App } from '../src/index';
 
-const canvas: HTMLCanvasElement = document.getElementById('app-canvas') as HTMLCanvasElement;
-const isDebug = true;
-const app = new App({
-	width: window.innerWidth,
-	height: window.innerHeight,
-	canvas: canvas,
-	isDebug: isDebug
-});
-app.start();
-
-window.addEventListener('resize', () => {
-	if (app) {
-		app.resize(window.innerWidth, window.innerHeight);
-	}
-});
-
-window.addEventListener('keydown', (ev: KeyboardEvent) => {
-	if (app) {
-		app.keydown(ev);
-	}
-});
+function main() {
+	const app = new App(){}
+}
--- src/@types/declaration.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.jpg';
-declare module '*.glsl' {
-	const glslData: string;
-	export default glslData;
-}
-declare module '*.json';
-declare module '*.html';
