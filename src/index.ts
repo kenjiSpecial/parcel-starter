be24cfe499230307547392348ab10a30a34c9afc
Kenji Saito <k.saito.1985@gmail.com>
2019-09-25 16:08:34 +0900

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-export class App {
-	constructor() {
-		console.log('hello?');
-	}
-}
+import firebase from 'firebase';
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp(config);
+
+new Vue({
+	el: '#app',
+	render: h => h(App)
+});
